695401d44ff297294f62c9a443fc0d45d5866be7
JonyFang <ahchunfang@gmail.com>
2017-05-08 20:34:24 +0800

    Remove abandoned print

--- guetzli_img_compression.py
+++ guetzli_img_compression.py
@@ -5,19 +5,10 @@ import sys
 import imghdr
 
 source = sys.argv[1]
-print('--source: ' + source)
 
 def convert_a_img(img_file):
-	print('--img_file: '+img_file)
-	print('--source: '+source)
-	# filename = os.path.splitext(os.path.split(img_file)[1])[0]
 	filename = os.path.split(img_file)[1]
-	print('--filename: '+filename)
-	# url = os.path.join(root, filename)
-	# print('--url: '+url)
 	url_out = os.path.join(source, '-'+filename)
-	print('--url_out: '+url_out)
-	print('---execute: ' + 'guetzli --quality 84 --verbose ' + img_file + ' ' + url_out)
 
 	subprocess.call(['guetzli', '--quality', '84', '--verbose', img_file, url_out])
 ###########################################################################
