63ceed37a04dc9ece927d2c8b2177b3fea3184a7
JonyFang <ahchunfang@gmail.com>
2017-05-08 20:42:37 +0800

    Configure TYPES

--- guetzli_img_compression.py
+++ guetzli_img_compression.py
@@ -5,6 +5,7 @@ import sys
 import imghdr
 
 source = sys.argv[1]
+TYPES = ('jpeg', 'png', 'jpg')
 
 def convert_a_img(img_file):
 	filename = os.path.split(img_file)[1]
@@ -18,7 +19,9 @@ def main():
 	if os.path.isdir(source):
 		for root, dirs, files in os.walk(source):
 			for a_file in files:
-				convert_a_img(os.path.join(root, a_file))
+				if imghdr.what(url) in TYPES:
+					convert_a_img(os.path.join(root, a_file))
+					pass
 
 	elif os.path.isfile(source):
 		convert_a_img(source)
