aa819095acf6947d423005f35a6f459ea9a7cb8b
JonyFang <ahchunfang@gmail.com>
2017-05-09 09:50:18 +0800

    Make output folder

--- guetzli_img_compression.py
+++ guetzli_img_compression.py
@@ -6,6 +6,11 @@ import sys
 source = sys.argv[1]
 TYPES = ('.jpeg', '.png', '.jpg')
 
+def ensure_dir():
+	dir_path = os.path.join(source, 'output')
+	if not os.path.exists(dir_path):
+		os.makedirs(dir_path)
+
 def convert_a_img(img_file):
 	file = os.path.split(img_file)[1]
 	filename = os.path.splitext(file)[0]
@@ -23,6 +28,7 @@ def main():
 			for a_file in files:
 				suffix = os.path.splitext(a_file)[1]
 				if suffix in TYPES:
+					ensure_dir()
 					convert_a_img(os.path.join(root, a_file))
 					pass
 
